a74f4ecc80b2d77b277d2f46c8f1bc0283cd3e81
Yağız Yılmaz <yagizzyilmazz21@hotmail.com>
2022-09-19 00:14:58 +0300

    Add project files.

---
+++ DataAccess/Migrations/20220918200345_v4.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class v4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MalzemeId",
+                table: "YemekAppTarifler",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MalzemeId",
+                table: "YemekAppTarifler",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
